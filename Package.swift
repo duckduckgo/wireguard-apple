274ae1c3dfc37730386d8682babda735c0444b94
Diego Rey Mendez <dreymendez@duckduckgo.com>
2026-03-03 22:15:27 +0100

    Update Package.swift for relay-poc-v4 release

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WireGuard",
-            url: "https://github.com/duckduckgo/wireguard-apple/releases/download/1.1.4-relay-poc-v3/WireGuard.xcframework.zip",
-            checksum: "df71ace095083a1259cc7739e2fa0510eb4035cbadbd6d3821d051315c459a69"
+            url: "https://github.com/duckduckgo/wireguard-apple/releases/download/1.1.4-relay-poc-v4/WireGuard.xcframework.zip",
+            checksum: "37cf93ac8cb05b2aacac93f8ab38f262f8a27022fc6e972a17238c1b6e44e1e4"
         ),
         .target(name: "_WireGuardDummy")
     ]
